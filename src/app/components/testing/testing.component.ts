f715823dc476c2ede5569d178116cb6dee9beab8
ejdeathcrash <ejdeathcrash@gmail.com>
2023-12-07 17:18:07 +0530

    first commit

---
+++ src/app/components/testing/testing.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-testing',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './testing.component.html',
+  styleUrl: './testing.component.css'
+})
+export class TestingComponent {
+
+}
